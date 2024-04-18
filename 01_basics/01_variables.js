7b281691d2c057b9759bdbb5279f6f224be7bc6d
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2024-04-18 11:54:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "anu@gmail.com";
+var accountPassword = "12345"; // var is old now
+accountCity = "Akola"; // not good way to write without let
+let accountstate; //undefined, value is not mentioned
+
+
+accountEmail = "a@gmail.com";
+accountPassword = "123";
+accountCity = "Pune";
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
