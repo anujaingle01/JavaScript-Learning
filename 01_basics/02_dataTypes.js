09e8beab9a66700d790f6a0d2dd7168d8feee8d8
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2024-04-18 12:18:23 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version. If not written still ok in new JS version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Anu")
+
+
+let name = "Anu" // string
+let age = 18    // number
+let isLoggedIn = false // boolean
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not mentioned or defined
+// symbol => unique
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
