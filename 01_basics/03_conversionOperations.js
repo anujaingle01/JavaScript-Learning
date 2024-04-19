b14cee32bf3e1499feaa38b51f5e540c3450fe50
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2024-04-19 10:53:52 +0000

    Conversion Operation

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,70 @@
+let score = "anuja"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "anu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+//  console.log(typeof booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "anu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);           // 2 to power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " anu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(7 + 5 + "1" + 2 + 2);  // add Things before String
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); // 1
+// console.log(+"");   // false or 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2      //4
+// console.log(num2);
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+
