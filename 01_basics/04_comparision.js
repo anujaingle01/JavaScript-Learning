f9e2bc4c6324d56d918279dce31b0edfd1032de6
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2024-04-19 11:47:46 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 10);
+
+
+// null is 0
+console.log(null > 0); //f
+console.log(null == 0); //f
+console.log(null >= 0); //t
+
+// undefined is False 
+console.log(undefined == 0); //f
+console.log(undefined > 0); //f
+console.log(undefined < 0); //f
+
+// ===         It Checks value + datatype
+// console.log("2" === 2);  // Data Type:     String != Number
