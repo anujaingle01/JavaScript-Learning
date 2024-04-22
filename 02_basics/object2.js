e9284a6c2e5e46c8efa77d6a824c4f302262e13e
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2024-04-22 20:00:21 +0000

    object 2

---
+++ 02_basics/object2.js
@@ -0,0 +1,84 @@
+// const tinderUser = new Object()      // constructor === singleton
+const tinderUser = {}                   // No constructor
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {           // nested objects
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Anuja",
+            lastname: "Ingle"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+const obj4 = { obj1, obj2 }     // object in object
+console.log(obj4);
+
+const obj5 = Object.assign({}, obj1, obj2, obj3)
+console.log(obj5);
+
+const obj6 = {...obj1, ...obj2}     // IMP : spread
+console.log(obj6);
+
+
+const users = [         // Array of Object
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));    // Array of array
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));   // T or F
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "anuja"
+}
+
+course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "anuja",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
