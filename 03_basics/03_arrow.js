a6bad127a053d5626aaf79f5c0a488d98d86fcfc
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2024-04-25 13:45:20 +0000

    arrow updated

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,53 +3,54 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        // console.log(`${this.username} , welcome to website`);
+        // console.log(this);
     }
 
 }
 
 // user.welcomeMessage()
-user.username = "sam"       // updated
-user.welcomeMessage()
+// user.username = "sam"       // updated
+// user.welcomeMessage()
 
-console.log(this);          //  {}
+// console.log(this);          //  {}
 
 
 function tea(){
     let username = "anu"
-    console.log(this.username);
+    // console.log(this.username);     // undefined
 }
 tea()       // undefined in function without storing in variable
 
 
 const chai = function () {
     let username = "anuja"
-    console.log(this.username);
+    // console.log(this.username);     // undefined
 }
-chai()      // undefined in function 
+chai()      // undefined in function with storing in variable
 
 
-const chai1 =  () => {
+
+const chai1 =  () => {          // arrow function
     let username = "anuja 1"
-    // console.log(this);
+    // console.log(this);          //  {}
 }
-// chai1()
+chai1()
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+
+const addTwoNo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwoNo(5, 7))
+
 
 // const addTwo = (num1, num2) =>  num1 + num2
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "anu"})
+console.log(addTwo(3, 4))
 
 
-// console.log(addTwo(3, 4))
-
-
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
+const myArray = [2, 5, 3, 7, 8]
+myArray.forEach(() => {})
