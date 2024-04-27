2116bf2be7aef27f67930d331ff2629c87630207
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2024-04-27 16:57:11 +0000

    Switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,49 @@
+
+// switch (key) {                   // Switch Case
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "march"       // String
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
+
+let no = 3;             // Number
+
+switch (no) {
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+
+    default:
+        console.log("Number Invalid")
+        break;
+}
