b0e6bc8e3a04918017791f679b051ba9a354024c
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2024-04-28 19:19:11 +0000

    map

---
+++ 05_iterations/map.js
@@ -0,0 +1,13 @@
+// ------------- Map -------------
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNums = myNumbers.map( (num) => { return num + 10})
+// console.log(newNums);
+
+
+// Chaining = Nested Methods
+const newNums1 = myNumbers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+console.log(newNums1);
